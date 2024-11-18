01222473712e70cfc2f1c6e873f7d00d99852ca0
asaad <asaadmohamed8756@gmail.com>
2024-11-18 16:45:41 +0100

    Add content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-[90%] mx-auto">
       <h1 className="text-3xl font-semibold">Next.js Opennext</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae autem ipsum pariatur libero illum molestias laborum ut atque eligendi. Voluptas, minima! Eligendi saepe dolorem velit eaque quae magni temporibus animi.
+      Tempora quam sed enim esse provident et rerum quo voluptate ea illum adipisci consequuntur tempore quae, aperiam quibusdam maiores consectetur soluta dolorum, aut obcaecati! Voluptatum aliquid optio eaque error maxime?</p>
+      <Button>Learn more</Button>
     </div>
   );
 }
